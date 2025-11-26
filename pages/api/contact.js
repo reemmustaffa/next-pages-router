990b832612652d9471b67ede494322c792431ed3
reem_mustafa <reemmustafa905@gmail.com>
2025-11-26 21:53:55 +0200

    API routes

---
+++ pages/api/contact.js
@@ -0,0 +1,31 @@
+import { supabase } from "@/lib/supabase";
+
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({
+      success: false,
+      message: "please make a POST request",
+    });
+  }
+  const contactData = {
+    fullName: "Reem",
+    email: "test",
+    subject: "Hello",
+    message: "Hey!",
+  };
+
+  const { error } = await supabase.from("contact").insert([contactData]);
+
+  if (error) {
+    res.status(500).json({
+      success: false,
+      message: "could not send message ! please try again",
+    });
+  }
+
+  //success Message
+  res.status(200).json({
+    success: true,
+    message: "Thank you for your message!we will be in touch soon!",
+  });
+}
